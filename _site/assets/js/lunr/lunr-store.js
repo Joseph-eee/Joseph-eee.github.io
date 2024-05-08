42396a2ba9a36d85a7169ef3f0215ec4e67a1d07
joseph-e-ee <kensigel.j@gmail.com>
2024-05-08 21:42:02 +1200

    change few option from config

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,1 +1,7 @@
-var store = []
+var store = [{
+        "title": "First Posting!",
+        "excerpt":"Welcome   Hello world,  ","categories": [],
+        "tags": [],
+        "url": "/first/",
+        "teaser": null
+      }]
